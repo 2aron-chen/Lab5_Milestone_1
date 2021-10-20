9d2f288bf7c788db58aa7b8be8bd7ed29ba1c5c0
kuanf <kuanfuchen88@gmail.com>
2021-10-20 16:07:06 -0500

    Add menu and function for logout.

--- app/src/main/java/com/example/lab5_milestone_1/MainActivity.java
+++ app/src/main/java/com/example/lab5_milestone_1/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.lab5_milestone_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onButtonClick(View view){
+        EditText myTextField = (EditText) findViewById(R.id.editTextTextPersonName);
+        String str = myTextField.getText().toString();
+
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String s){
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
